3c5bb950413579d9c589ac6c929cbd1785103676
aryanjais1234 <aryanjais1234@gmail.com>
2026-08-18 00:36:42 +0530

    updated-ingestion-service

--- ingestion-service/src/main/java/com/ingestion_service/controller/IngestionController.java
+++ ingestion-service/src/main/java/com/ingestion_service/controller/IngestionController.java
@@ -1,8 +1,10 @@
 package com.ingestion_service.controller;
 
+import com.clauseiq.AppConstants;
 import com.ingestion_service.dto.IngestResult;
 import com.ingestion_service.service.IngestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,5 +28,17 @@ public class IngestionController {
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Error uploading playbook: " + e.getMessage());
         }
+    }
+
+    @PostMapping("/contract")
+    public ResponseEntity<IngestResult> uploadContract(
+            @RequestParam("file") MultipartFile file,
+            @RequestHeader(AppConstants.HEADER_TENANT_ID) String tenantId)
+            throws IOException {
+
+        IngestResult result =
+                ingestionService.ingest(file, tenantId, "CONTRACT");
+
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 }
